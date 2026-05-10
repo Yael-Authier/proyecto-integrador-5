aee225285174acd6b5217387ed310fcf1f0181f8
Yael-Authier <yaelauthier@gmail.com>
2026-05-10 19:49:22 -0300

    V1.1.0 estructura inicial de feature engineering

---
+++ src/ft_engineering.py
@@ -0,0 +1,27 @@
+"""
+=================================================
+Feature Engineering y Preprocesamiento
+=================================================
+
+Objetivo:
+---------
+En este archivo se realizará la preparación de los datos
+para el entrenamiento de modelos de Machine Learning.
+
+Actividades principales:
+------------------------
+1. Tratamiento de valores nulos.
+2. Corrección de inconsistencias.
+3. Transformación de variables categóricas.
+4. Escalado y normalización.
+5. Creación de nuevas variables.
+6. Preparación final del dataset para modelado.
+
+Proyecto:
+----------
+Predicción de pago a tiempo en créditos.
+
+Autor:
+------
+Yael Authier
+"""
